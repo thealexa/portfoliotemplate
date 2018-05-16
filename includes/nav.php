d37451a5361003fd953b4b83861c981ffae1af9c
thealexa <36693033+thealexa@users.noreply.github.com>
2018-05-16 14:48:13 -0700

    Add files via upload

---
+++ includes/nav.php
@@ -0,0 +1,9 @@
+ <!--navigation-->
+    <nav class="topnav" id="myTopnav">
+            <a href="index.php" class="active">Home</a>
+            <a href="about.php">link2</a>
+            <a href="design.php">Link 3</a>
+            <a href="development.php">Link 4</a>
+            <a href="contact.php">Contact</a>
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+    </nav
